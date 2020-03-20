3113fb6a6c8e97e9ffa8f8bbcc7f383f8847af41
ritweek@rectorsol <info@rectorsol.com>
2020-03-20 13:08:29 +0530

    version1
    Api and Login system Done

---
+++ application/controllers/api/Login.php
@@ -0,0 +1,99 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+require APPPATH . 'libraries/REST_Controller.php';
+
+/**
+ * This is an example of a few basic user interaction methods you could use
+ * all done with a hardcoded array
+ *
+ * @package         CodeIgniter
+ * @subpackage      Rest Server
+ * @category        Controller
+ * @author          Phil Sturgeon, Chris Kacerguis
+ * @license         MIT
+ * @link            https://github.com/chriskacerguis/codeigniter-restserver
+ */
+class Login extends REST_Controller {
+
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+       
+        $this->load->helper('date');
+         $this->load->model('Common_model');
+        $this->load->helper('url');
+
+    }
+
+
+    public function index_post()
+    {
+        // $this->some_model->update_user( ... );
+        $message = [
+           
+            'email' => $this->post('email'),
+            'password' => $this->post('password'),
+            
+        ];
+        $str = $this->Common_model->Login_check($message);
+    
+        if($str) 
+       
+            {
+                
+                $str['message'] = 'User Found';
+                $data=[
+                    'user_id'=> $str[0]['logid'],
+                    'ip' => $_SERVER['REMOTE_ADDR'],
+                    'lastlog' => date('Y-m-d:h-m-s')
+                ];
+                 $this->Common_model->insert($data,'log');
+                $this->set_response($str, REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code
+            }
+         else{
+              $this->set_response("User Not Found", REST_Controller::HTTP_NO_CONTENT);
+         }
+     
+       
+    }
+  public function mobile_post()
+    {
+        
+        $message = [
+           
+            'mobile' => $this->post('mobile'),
+         
+        ];
+        
+        $str = $this->Common_model->Login_check_mobile($message);
+    
+        if($str==false) 
+       
+            {
+                $this->set_response("User Not Found", REST_Controller::HTTP_NO_CONTENT);
+               
+            }
+         else{
+              $str['message'] = 'User Found';
+                $data=[
+                    'id'=> $str[0]['logid'],
+                    'ip' => $this->post('ip'),
+                    'lastlog' => date('Y-m-d:h-m-s')
+                ];
+                
+                 $this->Common_model->insert($data,'log');
+                
+                 $str['otp'] =$data['code'];
+                $this->set_response($str, REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code 
+         }
+     
+       
+    }
+   
+
+}
---
+++ application/modules/auth/views/Login.php
@@ -0,0 +1,43 @@
+<?php include('layout/header.php'); ?>
+
+<body>
+    <!--[if lt IE 8]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+	<div class="error-pagewrap">
+		<div class="error-page-int">
+			<div class="text-center m-b-md custom-login">
+				<h3>PLEASE LOGIN TO APP</h3>
+				<p>This is the best app ever!</p>
+			</div>
+			<div class="content-error">
+				<div class="hpanel">
+                    <div class="panel-body">
+                        <form action="<?php echo base_url('auth/log')?>" Method="POST">
+                            <div class="form-group">
+                                <label class="control-label" for="username">Username</label>
+                                <input type="text"  required="" value="" name="username" id="username" class="form-control">
+                                <span class="help-block small">Your unique username to app</span>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label" for="password">Password</label>
+                                <input type="password" required="" value="" name="password" id="password" class="form-control">
+                                <span class="help-block small">Your strong password</span>
+                            </div>
+                            <div class="checkbox login-checkbox">
+                                <label>
+										<input type="checkbox" class="i-checks"> Remember me </label>
+                                <p class="help-block small">(if this is a private computer)</p>
+                            </div>
+                            <button class="btn btn-success btn-block loginbtn">Login</button>
+                           
+                        </form>
+                    </div>
+                </div>
+			</div>
+			<div class="text-center login-footer">
+				<p>Copyright © 2018. All rights reserved. Template by <a href="https://rectorsol.com/">Rector Sol</a></p>
+			</div>
+		</div>   
+    </div>
+  <?php include('layout/footer.php'); ?>
